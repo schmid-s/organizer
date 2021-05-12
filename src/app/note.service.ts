34e44e0d4a3a03c37e778268958553bc9966a04f
Sebastian Schmid <sp_schmid@yahoo.de>
2021-05-12 16:48:03 +0200

    add delete for single notes and whole topics

--- src/app/note.service.ts
+++ src/app/note.service.ts
@@ -15,12 +15,10 @@ import 'dexie-observable';
 })
 export class NoteService {
 
-  private topicsUrl = 'api/topics';
   private notesTable: Dexie.Table<Note, number>;
   private topicsTable: Dexie.Table<Topic, number>;
   private $noteId: Observable<number>;
   private $topicId: Observable<number>;
-  private params;
   private updatedNotes : Subject<Note[]> = new Subject<Note[]>();
   public notes : Observable<Note[]>;
   private currentTopicId : number;
@@ -37,10 +35,6 @@ export class NoteService {
     this.topicsTable = this.noteDBService.table('topics');
   }
 
-  //mitUpdatedNotes() : Observable<Note[]> {
-  //  this.currentNotes.next(from(this.notesTable.where("topicId").equals(topicId).toArray()));
-  //}
-
   getInitialNotes(topicId: number) : Promise<Note[]>{
     return this.notesTable.where("topicId").equals(topicId).toArray();
   }
@@ -56,44 +50,17 @@ export class NoteService {
     })
     //falls changes passieren erste subscription beenden und neu subscriben:
     this.noteDBService.on('changes', (changes) => {
-      //changes.forEach((change : any) => {
+      console.log('changes triggered');
+      subscription.unsubscribe();
+      subscription = from(this.notesTable.where("topicId").equals(topicId).toArray()).subscribe(data => {
+        this.updatedNotes.next(data);
         console.log('changes triggered');
-        subscription.unsubscribe();
-        subscription = from(this.notesTable.where("topicId").equals(topicId).toArray()).subscribe(data => {
-          this.updatedNotes.next(data);
-          console.log('changes triggered');
-        })
-        
-        
-        //this.updatedNotes.next(this.notesTable.where("topicId").equals(topicId).toArray());
-        /*
-        switch (change.type) {
-          
-          
-          case 1: // CREATED
-            console.log('An object was created: ' + JSON.stringify(change.obj));
-
-            break;
-          case 2: // UPDATED
-            console.log('An object with key ' + change.key + ' was updated with modifications: ' + JSON.stringify(change.mods));
-            break;
-          case 3: // DELETED
-            console.log('An object was deleted: ' + JSON.stringify(change.oldObj));
-            break;
-          
-        }
-        */
-      //});
+      })
     });
 
 
     console.log('noteservice performing getAllNotes on topicId: '+topicId);
     
-    //this.updatedNotes.next(from(this.getInitialNotes(topicId)));
-    //this.updatedNotes.subscribe((updatedNotes) => notes = of(updatedNotes));
-    //erst frage ich einfach den stand meines Observables ab
-    //falls es veränderungen gibt
-    //this.currentNotes.next(from(this.notesTable.where("topicId").equals(topicId).toArray()));
     return this.updatedNotes;
   }
 
@@ -117,6 +84,7 @@ export class NoteService {
   }
 
   updateNote(id, data) {
+    if(data.title === '') data.title = 'Unlabeled Note';
     return this.notesTable.update(id, data);
   }
 
@@ -124,6 +92,16 @@ export class NoteService {
     return this.notesTable.delete(id);
   }
 
+  async removeTopic(id) {
+    await this.deleteNotesByTopicId(id);
+    return this.topicsTable.delete(id);
+  }
+
+  async deleteNotesByTopicId(topicId): Promise<number>{
+    return await this.notesTable
+        .where("topicId").equals(topicId)
+        .delete();
+  }
 
   subscribeToCurrentNoteId(): void{
     this.route.paramMap.subscribe(params => {
@@ -140,8 +118,6 @@ export class NoteService {
     return note;
   }
 
-
-
   getCurrentTopic(topicId : number): Observable<Topic> {
     //let topic: Observable<Topic>;
     //this.$topicId.subscribe(id => {topic = from(this.topicsTable.get(id))});
@@ -157,23 +133,7 @@ export class NoteService {
   }
 
 
-
-
 ////////////////////////////////////////////in progress:
-
-  /*
-  getNote(topicId: number): Observable <Topic>{
-    const url = `${this.topicsUrl}/${topicId}`;
-    //const url = `./noteslist.${this.noteId}`;
-    return this.http.get<Topic>(url).pipe(
-      tap(_ => console.log(`fetched note id=${topicId}`)),
-      catchError(this.handleError<Topic>(`getTopic id=${id}`))
-    );
-  }
-  */
-  getParentTopic(route : ActivatedRoute){
-    
-  }
 
 
   /**
