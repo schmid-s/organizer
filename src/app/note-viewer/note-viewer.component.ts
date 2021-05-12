9b37a6e08c06ba6e227fb9bd0fef68ecf1c503e8
Sebastian Schmid <sp_schmid@yahoo.de>
2021-05-12 16:44:03 +0200

    add delete option for notes, fix autofocus, refactor

--- src/app/note-viewer/note-viewer.component.ts
+++ src/app/note-viewer/note-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild, QueryList, ViewChildren } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren, AfterViewInit, ElementRef } from '@angular/core';
 import { Note } from '../note';
 import {ActivatedRoute, Router, ParamMap, NavigationEnd} from '@angular/router';
 
@@ -6,26 +6,33 @@ import { Location} from '@angular/common';
 import { NoteService} from '../note.service';
 import { Topic } from '../topic';
 import { tap, switchMap } from 'rxjs/operators';
-import { throwError, iif, of } from 'rxjs';
+import { throwError, iif, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-note-viewer',
   templateUrl: './note-viewer.component.html',
   styleUrls: ['./note-viewer.component.scss']
 })
-export class NoteViewerComponent implements OnInit {
+export class NoteViewerComponent implements OnInit, AfterViewInit {
 
   topicId: number;
   topic: Topic;
   noteId: number;
-  note?: Note;
+  note?: Note = null;
 
-  //@ViewChild("noteTitle", {static: false}) titleInput: ElementRef;
-  @ViewChild("noteTitle", {static : false}) titleInput : any; //should any be exchanged for sth more specific? htmlelement does not work
+  @ViewChildren("titleInput") 
+  titleInput : QueryList<ElementRef>;
+
+  @ViewChildren("textInput") 
+  textInput : QueryList<ElementRef>;
 
   noteTextEmpty: boolean = false;
   noteTitleEmpty: boolean = false;
   newNoteState: boolean;
+  deleteDialog: boolean = false;
+
+  notesSubscription: Subscription;
+  titleInputChangesSubscription: Subscription;
 
   // parentId: number;
 
@@ -40,16 +47,22 @@ export class NoteViewerComponent implements OnInit {
 
   ngOnInit() {
     console.log('this.note is '+this.note);
-    //this.actOnRouteParams();
+    console.log('init-----------------------------------------------------------------');
     this.getNotesOnNewTopic();
+  }
+
+  ngAfterViewInit() {
+    this.manageFocusOnInputs();
     
   }
 
-  ngAfterViewInit(){
-    //if(this.newNoteState) this.setFocusToTitle();
-    //this.isNoteTitleEmpty();
-    //this.isNoteTextEmpty();
-    //this.setFocusToTitle();
+  ngOnDestroy() {
+    console.log('destroy-----------------------------------------------------------------');
+
+    // await this.checkForEmptyNoteTitle();
+
+    this.notesSubscription.unsubscribe();
+    this.titleInputChangesSubscription.unsubscribe();
   }
 
 /////////////////////////////////////////////////////
@@ -59,6 +72,7 @@ caseParamsHasNoNew(params){
   this.noteId = +params.get('noteId');
   console.log("Params Has No New")
   console.log("fetching note with id "+this.noteId )
+  // if(this.note) await this.checkForEmptyNoteTitle();
   return this.noteService.getNote(this.noteId)
 }
 
@@ -68,45 +82,21 @@ caseParamsHasNew(params){
 }
 
 getNotesOnNewTopic(){
-  this.route.paramMap.pipe(
+  this.notesSubscription = this.route.paramMap.pipe(
     tap((params) => this.topicId = +params.get('topicId')),
     switchMap(params =>
              iif(() => this.getCurrentRouteEnd() == 'new',
               of(params),
               this.caseParamsHasNoNew(params)
               )
-              /*
-              switchMap(params => 
-              
-                iif(() => params.has('new'),
-                this.caseParamsHasNew(params),
-                this.caseParamsHasNoNew(params)
-                )
-              )
-              */
-            
     )
   ).subscribe( (output) => {
-    /*
-    console.log('keys after subscribe = '+output.keys);
-    console.log('route.data = '+this.route.data);
-    console.log('newNoteState = '+this.route.snapshot.data.newNoteState);
-    console.log('url is = '+this.route.snapshot['_routerState'].url);
-    console.log('url by router = '+this.router.url.lastIndexOf('new'));
-    
-    console.log('url is = '+this.route.snapshot.url.lastIndexOf.toString);
-    console.log('queryParams = '+this.route.snapshot.queryParams['new']);
-    console.log('check for routerstate = '+this.route.snapshot['_routerState'].url.lastIndexOf('new'));
-    */
    console.log('route ending is = '+this.route.snapshot.url[this.route.snapshot.url.length -1]);
-   //console.log('url by router 2= '+this.router.url.includes('new'));
 
     if(output instanceof Note){
+
       if(this.noteId) {
-        this.note = output;
-        //this.setFocusToTitle();
-      
-      //console.log('this.note = '+this.note);
+        this.note = output;      
       } 
     }
     else if(this.instanceOfParamMap(output)){
@@ -115,12 +105,6 @@ getNotesOnNewTopic(){
     else{
       throwError;
     }
-    
-   /*
-   else{
-     this.createNewNote();
-   }
-   */
   });
 }
 
@@ -128,20 +112,24 @@ getNotesOnNewTopic(){
     return 'keys' in object;
   }
 
-  getCurrentRouteEnd(){
+  getCurrentRouteEnd() {
     return this.route.snapshot.url[this.route.snapshot.url.length -1].path;
   }
 
-  createNewNote(){
+  createNewNote(): void {
     console.log('create new note');
     this.note = new Note('');
     this.newNoteState = true;
     console.log('this.note.title is : '+this.note.title);
     this.checkIfInputsEmpty();
-    this.setFocusToTitle();
   }
 
-  
+  deleteNote(): Promise<any> {
+    let deleteMessage;
+    deleteMessage = this.noteService.removeNote(this.noteId);
+    console.log(deleteMessage);
+    return deleteMessage;
+  }
   
 /////////////////////////////////////////////////////////////
 
@@ -156,26 +144,38 @@ getNotesOnNewTopic(){
     this.isNoteTextEmpty();
   }
 
+  /* async checkForEmptyNoteTitle() {
+    if(this.note.title === '') {
+      await this.noteService.updateNote(this.noteId, {title : 'Unlabeled Note'})
+    }
+  } */
+
   isNoteTextEmpty() : void{
-    if (this.note.text == '') {
+    if (!this.note.text) {
       this.noteTextEmpty = true;
+      console.log(this.note);
+      console.log('this.noteTextEmpty = true');
     } else {
       this.noteTextEmpty = false;
+      console.log(this.note);
+      console.log('this.noteTextEmpty = false');
+
     }
   }
 
   isNoteTitleEmpty() : void{
-    if (this.note.title == '') {
+    if (!this.note.title) {
       this.noteTitleEmpty = true;
-      this.setFocusToTitle();
+      console.log('this.noteTitleEmpty = true');
     } else {
       this.noteTitleEmpty = false;
+      console.log('this.noteTitleEmpty = false');
     }
   }
 
   addNoteToDBOnFirstInput(){
-    let returnedNoteId;
     if(this.newNoteState){
+      let returnedNoteId;
       this.noteService.addNote({title : this.note.title, topicId : this.topicId})
         .then( (id) => {
           returnedNoteId = id;
@@ -184,18 +184,46 @@ getNotesOnNewTopic(){
           this.router.navigate(['notes-manager', this.topicId, 'notes', returnedNoteId]);
         }
       );
-      
+
     }
     else{
-      console.log("üüüüüüüüüüüüüüüüüüüüüüüüüüüü");
       this.noteService.updateNote(this.noteId, {title : this.note.title})
     }
   }
 
+  updateNoteByTextField() {
+    this.noteService.updateNote(this.noteId, {text : this.note.text})
+  }
+
+  manageFocusOnInputs() {
+    // titleInput element focussed when new Topic is accessed
+    if(this.getCurrentRouteEnd() == 'new') {
+      this.titleInput.first.nativeElement.focus();
+    }
+
+    // makes focus stay on titleImput element even when route has changed:
+    this.titleInputChangesSubscription = this.titleInput.changes.subscribe((list: QueryList<ElementRef>) => {
+      if (list.length > 0) {
+        list.first.nativeElement.focus();
+      }
+    });
+  }
+
+  /* 
+    Hintergrund zur Logik beim Fokus:
+
+    Der Fokus kann erst auf ngAfterViewInit gesetzt werden, wenn alle Dom Elemente geladen sind.
+    Im Fall der route endend auf /new funktioniert es, wenn es direkt in ngAfter ViewInit hook angegeben ist.
+
+    Im Fall der Route mit Parametern der Note Ids funktioniert es nicht, weil das Laden der DOM-Elemente
+    zusätzlich durch ein *ngIf verzögert wird. 
+   */
+
+  /*
   setFocusToTitle(){
     console.log("setFocusToTitle triggered");
     (document.activeElement as HTMLElement).blur();
-    //this.titleInput.nativeElement.focus();
+    this.titleInput.first.nativeElement.focus();
   }
-    
+  */
 }
